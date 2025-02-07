5c6284f37db8135704bcaa94856452af8bb165e4
gatodemontecristo <erickdevicente2024@outlook.com>
2025-02-06 22:18:08 -0500

    feat: added paragraph in the sotrybook

---
+++ src/components/atoms/Text/Paragraph.tsx
@@ -0,0 +1,41 @@
+import clsx from 'clsx';
+import { TailwindZeldaColor } from '../../../helpers';
+import React, { ReactElement } from 'react';
+
+interface ParagraphProps {
+  /**
+   * Children <p> elements
+   */
+  children?:
+    | ReactElement<HTMLParagraphElement>
+    | ReactElement<HTMLParagraphElement>[];
+  /**
+   * Font color
+   */
+  color?: TailwindZeldaColor;
+  /**
+   * Font size
+   */
+  size?: 'base' | 'lg' | 'xl';
+}
+export const Paragraph = ({
+  children,
+  color = 'zelda-sand',
+  size = 'base',
+}: ParagraphProps) => {
+  return (
+    <div className="flex flex-col text-center items-center justify-center w-full gap-3">
+      {React.Children.map(children, (child) =>
+        child
+          ? React.cloneElement(child, {
+              className: clsx(
+                'italic font-light p-4 w-[60%]',
+                'text-' + color,
+                'text-' + size,
+              ),
+            })
+          : null,
+      )}
+    </div>
+  );
+};
--- src/components/atoms/Text/index.ts
+++ src/components/atoms/Text/index.ts
@@ -1,1 +1,2 @@
 export * from './SubTitle';
+export * from './Paragraph';
