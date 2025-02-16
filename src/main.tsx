abd554f130f6ac8ed0a769b126b2d1490356f287
gatodemontecristo <erickdevicente2024@outlook.com>
2025-02-16 14:19:17 -0500

    feat: installed tanstack 🏝️

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { ZeldaApp } from './ZeldaApp.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ZeldaApp />
+    <QueryClientProvider client={queryClient}>
+      <ZeldaApp />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
